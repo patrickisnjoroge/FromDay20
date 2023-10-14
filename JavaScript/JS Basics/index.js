a1450c3a13ec49f34c345c330f384494a3ebbe86
patrickisnjoroge <patrickisnjoroge@gmail.com>
2023-10-14 12:55:48 +0300

    Day 21

---
+++ JavaScript/JS Basics/index.js
@@ -0,0 +1,22 @@
+// // DAY 21
+// // EXERCISES
+// // Exercise 1: ARRAY FROM RANGE
+// const numbers = arrayFromRange(-8, -3);
+
+// console.log(numbers);
+
+// function arrayFromRange(min, max) {
+//   const output = [];
+//   for (let i = min; i <= max; i++) output.push(i);
+//   return output;
+// }
+
+// // Exercise 2. INCLUDES
+// const numbers = [1, 2, 3, 4];
+
+// console.log(includes(numbers, 1));
+
+// function includes(array, searchElement) {
+//   for (let element of array) if (element === searchElement) return true;
+//   return false;
+// }
---
+++ Python/HelloWorld/app.py
@@ -0,0 +1,43 @@
+# # DAY 21
+# # 2. LOOPS
+# # FOR ELSE LOOP
+# successful = True
+# for number in range(3):
+#     print("Attempt")
+#     if successful:
+#         print("Successful")
+#         break
+# else:
+#     print("attempted but failed 3 times")
+
+# # 3. NESTED LOOPS
+# for x in range(5):
+#     for y in range(3):
+#         print(f"{x}, {y}")
+
+# # 4. ITERABLES
+# count = 0
+# for number in range(1, 10):
+#     if number % 2 == 0:
+#         count += 1
+#         print(number)
+# print(f"We have {count} even numbers")
+
+
+# FUNCTIONS
+# DEFINING A FUNCTION
+# def greet():
+#     print("Hello World")
+#     print("Welcome Aboard")
+
+
+# greet()
+
+# # ARGUMENTS
+# def greet(first_name, last_name):
+#     print(f"Hello {first_name} {last_name}")
+#     print("Welcome Aboard")
+
+
+# greet("Parel", "Gorgeous")
+# greet("Houdini", "Hadani")
